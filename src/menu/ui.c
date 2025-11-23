bc4a84d1f82b82c1dd6c91ade6fd5551c43971c5
vexalous <vexalous@hotmail.com>
2025-11-23 11:07:29 -0500

    Change menu navigation instructions from Up/Down to w/s

--- src/menu/ui.c
+++ src/menu/ui.c
@@ -10,7 +10,7 @@ void draw_menu(WINDOW *w,int hl){
     if(!w) return;
     werase(w); box(w,0,0);
     mvwprintw(w,1,2,"%s",MENU_TITLE);
-    mvwprintw(w,2,2,"Use Up/Down to move, Enter to select");
+    mvwprintw(w,2,2,"Use w/s to move, Enter to select");
     for(int i=0,y=5;i<3;++i){
         if(i==hl) wattron(w,A_REVERSE|A_BOLD);
         mvwprintw(w,y+i,4,"%s",menu_items[i]);
