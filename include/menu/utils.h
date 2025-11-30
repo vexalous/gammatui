b04c609ad05462801155a5c83d496f954103d28f
vexalous <vexalous@hotmail.com>
2025-11-30 17:18:08 -0500

    Update utils.h

--- include/menu/utils.h
+++ include/menu/utils.h
@@ -7,5 +7,3 @@ bool is_executable_file(const char *path);
 bool resolve_exe_dir(char *out, size_t outlen, const char *argv0);
 bool build_gammagui_path(char *out, size_t outlen, const char *argv0);
 bool build_settings_path(char *out, size_t outlen, const char *argv0);
-
-#endif
