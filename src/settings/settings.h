6c27ff9f0ffe75a9d3a697b6f74a415f03043818
vexalous <vexalous@hotmail.com>
2025-12-02 09:38:01 -0500

    Update settings.h

--- src/settings/settings.h
+++ src/settings/settings.h
@@ -1,30 +1,20 @@
-#ifndef GAMMATUI_SETTINGS_H
-#define GAMMATUI_SETTINGS_H
-
-#define _POSIX_C_SOURCE 200809L
+#ifndef S_H
+#define S_H
 #include <stddef.h>
-#include <limits.h>
 #include <stdbool.h>
 
 #define OUTPUT_LEN 128
 
 struct cfg {
+    double gamma_min, gamma_max, bright_min, bright_max;
     char output[OUTPUT_LEN];
-    double gamma_min;
-    double gamma_max;
-    double bright_min;
-    double bright_max;
-    int sel_r, sel_g, sel_b;
-    int unsel_r, unsel_g, unsel_b;
-    int key_up;
-    int key_down;
-    int key_select;
-    int key_quit; 
+    int sel_r, sel_g, sel_b, unsel_r, unsel_g, unsel_b;
+    int key_up, key_down, key_select, key_quit;
 };
 
-bool config_path_for_exe(char *out, size_t outlen, const char *argv0);
-void config_set_defaults(struct cfg *c);
-bool load_config(struct cfg *c, const char *path);
-bool save_config(const struct cfg *c, const char *path);
+bool config_path_for_exe(char *, size_t, const char *);
+void config_set_defaults(struct cfg *);
+bool load_config(struct cfg *, const char *);
+bool save_config(const struct cfg *, const char *);
 
 #endif
